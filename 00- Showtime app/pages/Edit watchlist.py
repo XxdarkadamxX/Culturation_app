db65a2d3cc8e3ede480b26a1eaf117ba3cf80b4d
Adam Mharzi <adamharzi@gmail.com>
2025-08-11 04:16:12 +0200

    add a way to remove moviesfrom watchlist

--- 00- Showtime app/pages/Edit watchlist.py
+++ 00- Showtime app/pages/Edit watchlist.py
@@ -8,8 +8,32 @@ watchlist_csv_path = Path(r"C:\Users\adamh\OneDrive\Bureau\Cinema showtime app\C
 
 watchlist_df = pd.read_csv(watchlist_csv_path)
 
-st.write(watchlist_df)
 
+
+if not watchlist_df.empty:
+    st.write("### Current watchlist")
+
+    edited_df = st.data_editor(
+        watchlist_df,
+        use_container_width=True,
+        hide_index=True,
+        column_config={
+            "Watched": st.column_config.CheckboxColumn(
+                "Watched",
+                help="Mark as watched to remove",
+                default=False
+            )
+        },
+        disabled=[col for col in watchlist_df.columns if col != "Watched"]  # make all other columns read-only
+    )
+
+    if st.button("Remove watched movies"):
+        updated_df = edited_df[~edited_df["Watched"].fillna(False)]
+        updated_df.to_csv(watchlist_csv_path, index=False)
+        st.success("Removed watched movies from the list.")
+        st.rerun()
+else:
+    st.write("No movies in your watchlist.")
 
 st.write("Add a movie")
 
@@ -26,8 +50,9 @@ if add and (french_title=='' and english_title=='') :# If the movie name isn't g
 elif add and (french_title in watchlist_df['Nom Francais'].values or english_title in watchlist_df['Nom Anglais'].values):
     st.error("ya deja le film mon gars concentres toi")
 elif add and (french_title !='' or english_title !='') :
-    watchlist_df.loc[len(watchlist_df)] = [french_title, english_title,"",trailer]
+    watchlist_df.loc[len(watchlist_df)] = [french_title, english_title,"",trailer,False]
     watchlist_df.to_csv(watchlist_csv_path, index=False)
+    st.rerun()
     st.success("bien joué bg")
 
 st.write(watchlist_df)
