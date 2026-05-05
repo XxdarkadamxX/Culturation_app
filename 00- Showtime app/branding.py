9833b7b5121b73f0b42a4639f87b145ffc01fb6d
Adam Mharzi <adamharzi@gmail.com>
2026-05-05 19:03:04 +0200

    add bigger logo

--- 00- Showtime app/branding.py
+++ 00- Showtime app/branding.py
@@ -9,4 +9,6 @@ LOGO_PATH = PROJECT_ROOT / "Logo sidebar.png"
 
 def apply_branding() -> None:
     if LOGO_PATH.exists():
-        st.logo(str(LOGO_PATH),size='large')
+        st.sidebar.image(str(LOGO_PATH), width='stretch')
+        st.logo(str(LOGO_PATH), size="large", icon_image=str(LOGO_PATH))
+        
